2a329c1af101b82281f1ab4fe52fb80d6bc73832
Ndubi Amos <17646775+ndubiamos@users.noreply.github.com>
2023-10-29 13:54:56 +0300

    Update About.js

--- components/About.js
+++ components/About.js
@@ -36,7 +36,7 @@ const About = (props) => {
     },
     {
       name: 'Eskin John',
-      image: '/eskin.png',
+      image: '/Eskin.png',
       socialLinks: [
         {
           name: 'Twitter',
