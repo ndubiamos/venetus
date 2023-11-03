054792b0af984ac6f4c988e75c4c8f2cef90ff11
Ndubi Amos <17646775+ndubiamos@users.noreply.github.com>
2023-11-03 17:19:28 +0300

    Update Settings.js

--- components/Settings.js
+++ components/Settings.js
@@ -3,6 +3,11 @@ import styles from "./../styles/Settings.module.css";
 import About from "./About";
 
 const Settings = (props) => {
+	 const [author, setAuthor] = useState(false);
+
+  	const handleAbout = () => {
+   	 setAuthor(true);
+ 	 };
 	return (
 		<div className={styles.settings}>
 			<div className={styles.nav}>
@@ -12,6 +17,7 @@ const Settings = (props) => {
 			<div className={styles.all}>
 				<div className={styles.item} onClick={() => props.setOn(true)}><span>Home</span> <img src="enter.svg"/></div>
 				<div className={styles.item} onClick={() => props.setReset(true)}><span>Update region</span> <img src="enter.svg"/></div>
+				{setAuthor && <About handleAbout={() => setAuthor(false)} />}
 				<div className= {styles.item} onClick={()=> props.setAuthor(true)}><span>About us</span> <img src="enter.svg"/></div>
 				<a href="https://github.com/ndubiamos/venetus#credit" target="_blank" className={styles.item}><span>Credit</span> <img src="enter.svg"/></a>
 				<div className={styles.space}></div>
