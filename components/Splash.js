6d5434929ab10735ef46bdee06d139c909f8fe20
Ndubi Amos <17646775+ndubiamos@users.noreply.github.com>
2023-11-03 16:46:38 +0300

    Update Splash.js

--- components/Splash.js
+++ components/Splash.js
@@ -121,9 +121,6 @@ const Splash = (props) => {
           <div className={styles.button} onClick={() => setGetStarted(true)}>
             Get started
           </div>
-          <div className={styles.button} onClick={() => setAuthor(true)}>
-            About
-          </div>
         </div>
       )}
     </div>
